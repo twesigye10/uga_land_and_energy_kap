5eec69dfa8baf316bbc678f052973dd9921529f4
Anthony Twesigye <twesigye10@gmail.com>
2023-09-01 11:08:58 +0300

    adding time comparisons between main and audit files

--- R/working_with_audit_files.R
+++ R/working_with_audit_files.R
@@ -3,11 +3,15 @@ library(lubridate)
 
 # main data ---------------------------------------------------------------
 
-df_main_data <- readxl::read_excel("inputs/UGA2305_land_and_energy_data.xlsx")
+df_main_data <- readxl::read_excel("inputs/UGA2305_land_and_energy_data.xlsx") |> 
+    mutate(start = lubridate::as_datetime(start),
+           end = lubridate::as_datetime(end),
+           main_survey_time_interval = lubridate::time_length(end - start, unit = "min"), 
+           main_survey_time_interval = ceiling(main_survey_time_interval))
 
 # ********************* consider data that is not in deletion log // to be handled later
 df_main_data_support_audit <- df_main_data |> 
-    select(uuid = `_uuid`, enumerator_id, district_name, point_number)
+    select(uuid = `_uuid`, enumerator_id, district_name, point_number, main_survey_time_interval)
 
 # read audit files --------------------------------------------------------
 
@@ -24,7 +28,9 @@ df_audit_data <- df_audit_raw |>
            i.check.qn_time_interval = round(x = (end - start)/1000, digits = 0)) |> 
     supporteR::batch_select_rename()
 
-# outliers in the audit data
+
+# outliers in the audit data ----------------------------------------------
+
 df_check_audit_outliers <- cleaningtools::check_outliers(dataset = df_audit_data |> mutate(audit_uuid = paste0(audit_uuid, " * ", audit_qn)), 
                                                          uuid_column = "audit_uuid", strongness_factor = 3) 
 
@@ -47,16 +53,26 @@ df_time_interval_outliers_per_enumerator <- df_potential_audit_outliers |>
 
 # outlier issues per location// also group by host or settlement
 
+# mean audit time ---------------------------------------------------------
+
 # mean question times per enumerator//outliers
 df_qn_time_enum_means <- df_audit_data |> 
     left_join(y = df_main_data_support_audit, by = c("audit_uuid" = "uuid")) |>
     group_by(enumerator_id, audit_qn) |>
     summarise(enum_qn_average_time = round(x = mean(qn_time_interval, na.rm = TRUE), digits = 0))
 
+# main survey and audit time comparison -----------------------------------
+df_main_and_audit_times <- df_audit_data |> 
+    group_by(audit_uuid) |>
+    summarise(audit_survey_time_interval = sum(qn_time_interval, na.rm = TRUE) / 60) |> 
+    left_join(y = df_main_data_support_audit, by = c("audit_uuid" = "uuid")) |>
+    mutate()
+
 list_audit_summary_data <- list("interval outliers" = df_potential_audit_outliers,
                            "interval outliers per survey" = df_time_interval_outliers_per_survey,
                            "interval outliers per enum" = df_time_interval_outliers_per_enumerator,
-                           "average time per qn and enum" = df_qn_time_enum_means)
+                           "average time per qn and enum" = df_qn_time_enum_means,
+                           "time diff main and audit" = df_main_and_audit_times)
 
 openxlsx::write.xlsx(x = list_audit_summary_data,
                      file = paste0("outputs/", butteR::date_file_prefix(), 
