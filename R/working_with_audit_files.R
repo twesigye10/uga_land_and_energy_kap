44e2898d229549f9f3cd8fc1aac806abcfb44b60
Anthony Twesigye <twesigye10@gmail.com>
2023-09-12 16:37:01 +0300

    updated audit files

--- R/working_with_audit_files.R
+++ R/working_with_audit_files.R
@@ -1,5 +1,11 @@
 library(tidyverse)
 library(lubridate)
+library(openxlsx)
+
+# global options can be set to further simplify things
+options("openxlsx.borderStyle" = "thin")
+# options("openxlsx.borderColour" = "#4F81BD")
+options("openxlsx.withFilter" = TRUE)
 
 # main data ---------------------------------------------------------------
 
@@ -23,8 +29,8 @@ df_audit_raw <- readr::read_csv(my_audit_list, id = "file_name")
 df_audit_data <- df_audit_raw |> 
     filter(event %in% c("question")) |> 
     mutate(i.check.audit_uuid = str_replace_all(string = file_name, pattern = "^inputs\\/audit_files\\/|\\/audit\\.csv$", replacement = ""),
-           # i.check.start = start,
-           # i.check.end = end,
+           i.check.audit_start = as_datetime(as.POSIXct(start/1000, origin = "1970-01-01", tz = "Africa/Nairobi")),
+           i.check.audit_end = as_datetime(as.POSIXct(end/1000, origin = "1970-01-01", tz = "Africa/Nairobi")),
            i.check.audit_qn = str_extract(string = node, pattern = "[\\w]+$"),
            i.check.qn_time_interval = round(x = (end - start)/1000, digits = 0)) |> 
     supporteR::batch_select_rename()
@@ -37,6 +43,7 @@ df_check_audit_outliers <- cleaningtools::check_outliers(dataset = df_audit_data
 
 df_potential_audit_outliers <- df_check_audit_outliers$potential_outliers|> 
     separate_wider_delim(cols = uuid, delim = " * ", names = c("audit_uuid", "audit_qn")) |> 
+    filter(!str_detect(string = audit_qn, pattern = "_note$|_other$")) |> 
     select(-question) |> 
     rename(qn_time_interval = old_value)
 # cols to add enumerator, district, location/settlement
@@ -70,14 +77,88 @@ df_main_and_audit_times <- df_audit_data |>
     mutate(main_and_audit_timme_diff = main_survey_time_interval - audit_survey_time_interval) |> 
     relocate(audit_survey_time_interval, .after = point_number)
 
-# list of datasets
-list_audit_summary_data <- list("interval outliers" = df_potential_audit_outliers,
-                           "interval outliers per survey" = df_time_interval_outliers_per_survey,
-                           "interval outliers per enum" = df_time_interval_outliers_per_enumerator,
-                           "average time per qn and enum" = df_qn_time_enum_means,
-                           "time diff main and audit" = df_main_and_audit_times)
 
-openxlsx::write.xlsx(x = list_audit_summary_data,
+# export audit data -------------------------------------------------------
+
+openxlsx::write.xlsx(x = df_audit_data,
                      file = paste0("outputs/", butteR::date_file_prefix(), 
-                                   "_audit_summary_data.xlsx"), 
+                                   "_audit_raw_data_land_energy.xlsx"), 
                      overwrite = TRUE, keepNA = TRUE, na.string = "")
+
+# formatting --------------------------------------------------------------
+
+wb <- createWorkbook()
+
+hs1 <- createStyle(fgFill = "#EE5859", halign = "CENTER", textDecoration = "Bold", fontColour = "white", fontSize = 14, wrapText = T)
+hs2 <- createStyle(fgFill = "#808080", halign = "CENTER", textDecoration = "Bold", fontColour = "white", wrapText = T)
+hs3 <- createStyle(fgFill = "#EE5859", halign = "CENTER", textDecoration = "Bold", border = "Bottom", fontColour = "white")
+rowhs <- createStyle(fontColour = "#9C0006", bgFill = "#FFC7CE")
+
+# write data
+var_sheet_name <- "time diff main and audit"
+addWorksheet(wb, sheetName = var_sheet_name)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 1, widths = 37)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 2:4, widths = 18)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 5:7, widths = 30)
+writeDataTable(wb = wb, 
+               sheet = var_sheet_name, 
+               x = df_main_and_audit_times, 
+               startRow = 1, 
+               startCol = 1, 
+               tableStyle = "TableStyleLight9", 
+               headerStyle = hs3)
+conditionalFormatting(wb = wb, sheet = var_sheet_name, cols=1:7,rows = 2:nrow(df_main_and_audit_times), rule="AND($E2<20, $F2>20)", style = rowhs)
+
+var_sheet_name <- "average time per qn and enum"
+addWorksheet(wb, sheetName = var_sheet_name)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 1, widths = 18)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 2, widths = 56)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 3, widths = 26)
+writeDataTable(wb = wb, 
+               sheet = var_sheet_name, 
+               x = df_qn_time_enum_means, 
+               startRow = 1, 
+               startCol = 1, 
+               tableStyle = "TableStyleLight9", 
+               headerStyle = hs3)
+
+var_sheet_name <- "interval outliers"
+addWorksheet(wb, sheetName = var_sheet_name)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 1, widths = 37)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 2, widths = 50)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 3, widths = 30)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 4, widths = 20)
+writeDataTable(wb = wb, 
+               sheet = var_sheet_name, 
+               x = df_potential_audit_outliers, 
+               startRow = 1, 
+               startCol = 1, 
+               tableStyle = "TableStyleLight9", 
+               headerStyle = hs3)
+
+var_sheet_name <- "interval outliers per survey"
+addWorksheet(wb, sheetName = var_sheet_name)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 1, widths = 37)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 2, widths = 36)
+writeDataTable(wb = wb, 
+               sheet = var_sheet_name, 
+               x = df_time_interval_outliers_per_survey, 
+               startRow = 1, 
+               startCol = 1, 
+               tableStyle = "TableStyleLight9", 
+               headerStyle = hs3)
+
+var_sheet_name <- "interval outliers per enum"
+addWorksheet(wb, sheetName = var_sheet_name)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 1, widths = 18)
+setColWidths(wb = wb, sheet = var_sheet_name, cols = 2, widths = 40)
+writeDataTable(wb = wb, 
+               sheet = var_sheet_name, 
+               x = df_time_interval_outliers_per_enumerator, 
+               startRow = 1, 
+               startCol = 1, 
+               tableStyle = "TableStyleLight9", 
+               headerStyle = hs3)
+
+saveWorkbook(wb, paste0("outputs/", butteR::date_file_prefix(),"_formatted_audit_summary_land_and_energy.xlsx"), overwrite = TRUE)
+openXL(file = paste0("outputs/", butteR::date_file_prefix(),"_formatted_audit_summary_land_and_energy.xlsx"))
