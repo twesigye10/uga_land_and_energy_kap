63dbfa7e9330bdba49819e133ae42cbe30c2bf2e
Anthony Twesigye <twesigye10@gmail.com>
2023-11-03 11:04:15 +0300

    added file: composite_indicators.R

---
+++ R/composite_indicators.R
@@ -0,0 +1,11 @@
+# function for creating composite indicators
+create_composite_indicators <- function(input_df) {
+    input_df %>% 
+        mutate(i.meta_region = case_when(meta_district_name %in% c("isingiro", "kamwenge", "kikuube", "kyegegwa") ~ "south_west",
+                                         TRUE ~ "west_nile"),
+               i.meta_respondent_age = case_when(meta_respondent_age <= 18 ~ "age_12_18",
+                                                 meta_respondent_age <= 59 ~ "age_19_59",
+                                                 meta_respondent_age > 59 ~ "age_greater_59",
+                                                 TRUE ~ "NA")
+        )
+}
